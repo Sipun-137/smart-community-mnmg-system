30252a74a8099f0be584d75f6233465852173cb6
Sipun_137 <subhranshu060@gmail.com>
2025-03-21 19:37:13 +0530

    Utils File Moved

--- src/services/ParkingService.ts
+++ src/services/ParkingService.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import { base_url } from "@/utils/lib";
+import { base_url } from "@/utils";
 import axios from "axios";
 import Cookies from "js-cookie";
 
