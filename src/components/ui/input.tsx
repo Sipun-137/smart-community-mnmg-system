63faf4ffffe920c874ec91b9dbb5daa6e3ccfb88
Sipun_137 <subhranshu060@gmail.com>
2025-03-21 19:39:00 +0530

    Component Added From shadcn library

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,8 +1,8 @@
 // Input component extends from shadcnui - https://ui.shadcn.com/docs/components/input
 "use client";
 import * as React from "react";
-import { cn } from "@/utils/lib/utils";
 import { useMotionTemplate, useMotionValue, motion } from "framer-motion";
+import { cn } from "@/lib/utils";
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export interface InputProps
